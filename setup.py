cf77e7e4617d781afef2be4235b863b6e0251922
Katrina Chan <katrina.hyc@gmail.com>
2019-09-22 22:45:22 +0800

    Update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='formosa',
-    version='0.1.0',
+    version='0.1.1',
     author='Hao-Yung Chan',
     author_email='katrina.hyc@gmail.com',
     packages=['formosa'],
