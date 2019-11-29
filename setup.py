16dbeb25d7fa2de36cf691bc23ab54561251d89f
Katrina Chan <katrina.hyc@gmail.com>
2019-11-30 01:55:33 +0800

    Update version; add a requirement

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@ from distutils.core import setup
 
 setup(
     name='formosa',
-    version='0.1.1',
+    version='0.2.0',
     author='Hao-Yung Chan',
     author_email='katrina.hyc@gmail.com',
     packages=['formosa'],
     license='LICENSE',
-    description='Coloring SVG Taiwan map.',
+    description='Generate and color SVG Taiwan maps.',
     long_description=open('README.md').read(),
     install_requires=[
-        'svgwrite'
+        'svgwrite',
+        'pyshp',
     ],
 )
